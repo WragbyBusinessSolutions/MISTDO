8ffd0966b0122578830aa9cc59850b270f9641aa
Tolulope Agabielesin <tagabielesin@wragbysolutions.com>
2018-10-09 18:28:21 +0100

    initial create and API

---
+++ MISTDO.Api/Models/RegisterResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MISTDO.Api.Models
+{
+    public class RegisterResponse
+    {
+        public string UserId { get; set; }
+        public string token { get; set; }
+        public string scheme { get; set; }
+
+    }
+}
---
+++ MISTDO.Api/ViewModels/GenericViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MISTDO.Api.ViewModels
+{
+    public class GenericViewModel
+    {
+        public string Message { get; set; }
+    }
+}
