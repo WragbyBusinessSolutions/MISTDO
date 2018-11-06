75410856b161237617894adb56516e1291ac2e1c
Tolulope Agabielesin <Tagabielesin@wragbysolutions.com>
2018-11-06 10:40:06 +0100

    updates

--- MISTDO.Web/wwwroot/dist/assets/js/dashboard/dashboard.js
+++ MISTDO.Web/wwwroot/dist/assets/js/dashboard/dashboard.js
@@ -3,19 +3,19 @@
 (function ($) {
 	'use strict';
 
-	var primary = '#7774e7',
-		success = '#37c936',
-		info = '#0f9aee',
-		warning = '#ffcc00',
-		danger = '#ff3c7e',
-		primaryInverse = 'rgba(119, 116, 231, 0.1)',
-		successInverse = 'rgba(55, 201, 54, 0.1)',
-		infoInverse = 'rgba(15, 154, 238, 0.1)',
-		warningInverse = 'rgba(255, 204, 0, 0.1)',
-		dangerInverse = 'rgba(255, 60, 126, 0.1)',
-		gray = '#f6f7fb',
-		white = '#fff',
-		dark = '#515365'
+    var primary = '#7774e7',
+        success = '#37c936',
+        info = '#0f9aee',
+        warning = '#ffcc00',
+        danger = '#ff3c7e',
+        primaryInverse = 'rgba(119, 116, 231, 0.1)',
+        successInverse = 'rgba(55, 201, 54, 0.1)',
+        infoInverse = 'rgba(15, 154, 238, 0.1)',
+        warningInverse = 'rgba(255, 204, 0, 0.1)',
+        dangerInverse = 'rgba(255, 60, 126, 0.1)',
+        gray = '#f6f7fb',
+        white = '#fff',
+        dark = '#515365';
 
     $('#monthly-target').vectorMap({
         map: 'us_aea',
@@ -137,7 +137,7 @@
                   stacked: true,
                    ticks: {
                       min: 0,
-                      stepSize: 30,
+                       stepSize: 30
                   }
 
               }]
