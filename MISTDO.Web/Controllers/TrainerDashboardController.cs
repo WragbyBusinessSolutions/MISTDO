8a614850df4011360871d1d99f3d92468b7492e9
Femi Ogunyomi <fogunyomi@wragbysolutions.com>
2019-02-08 13:58:49 +0100

    dfdp removed

--- MISTDO.Web/Controllers/TrainerDashboardController.cs
+++ MISTDO.Web/Controllers/TrainerDashboardController.cs
@@ -22,16 +22,7 @@ using System.Diagnostics;
 using System.Text.Encodings.Web;
 
 //FingerPrint Assembly 
-using DPFP;
-using DPFP.Capture;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+
 
 
 namespace MISTDO.Web.Controllers
