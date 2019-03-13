4aaea711cd8713687d02ba5e8597295cd9b525a4
Femi Ogunyomi <fogunyomi@wragbysolutions.com>
2019-03-13 12:57:37 +0100

    Training Providers

---
+++ MISTDO.Web/Migrations/20190313114743_upodates.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MISTDO.Web.Migrations
+{
+    public partial class upodates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
