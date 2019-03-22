f1d25721e9f499712f0d44cd0190b86350aa73af
Femi Ogunyomi <fogunyomi@wragbysolutions.com>
2019-03-22 11:02:33 +0100

    Admin generating ID Cards

--- MISTDO.Web/Controllers/AdministratorController.cs
+++ MISTDO.Web/Controllers/AdministratorController.cs
@@ -25,6 +25,7 @@ using System.Security.Claims;
 using MISTDO.Web.Models.AccountViewModels;
 using System.Net.Mail;
 using System.Net;
+using MISTDO.Web.ViewModels;
 
 namespace MISTDO.Web.Controllers
 { 
@@ -279,6 +280,214 @@ namespace MISTDO.Web.Controllers
 
             return View(calenders);
         }
+        public async Task<IActionResult> Modules()
+        {
+            var modules = await _trainer.GetAllModules();
+
+            var modulesList = new List<SelectListItem>();
+            foreach (var item in modules)
+
+                modulesList.Add(new SelectListItem { Text = item.Name, Value = item.Id.ToString() });
+
+
+            ViewBag.modules = modulesList;
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Modules(Modules model,string trainer)
+        {
+
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(EligibleUsersForCertificate), new { ModuleId = model.Id, trainerid = trainer });
+            }
+            return View(model);
+        }
+        public async Task<IActionResult> EligibleUsersForCertificate(int ModuleId, string trainerid)
+        {
+            var users = new List<SelectListItem>();
+
+            var us =  _usermanager.Users.FirstOrDefault(t => t.UID == trainerid);
+             var Trainer = await _usermanager.FindByIdAsync(us.Id);
+            
+
+            //   var traineesList = new List<TraineeViewModel>();
+            var trainings = await _trainer.GetNullCertificateTrainees(Trainer.Id, ModuleId.ToString());
+
+            foreach (var trainee in trainings)
+            {
+                var TraineeApplicationUser = Traineedbcontext.Users.Where(t => t.UID == trainee.TraineeId).ToList();
+
+                foreach (var user in TraineeApplicationUser)
+                {
+                    users.Add(new SelectListItem { Text = user.UserName, Value = user.Id });
+
+                }
+
+            }
+            ViewBag.users = users;
+            ViewBag.Trainer = Trainer.Id;
+
+            ViewBag.ModuleId = ModuleId;
+
+            return View();
+
+
+        }
+
+
+        [HttpPost]
+
+        public IActionResult EligibleUsersForCertificate(NewCertificateViewModel model,string trainer)
+        {
+            if (model.TraineeId == null)
+            {
+                return Content("No Available Trainee");
+            }
+            if (ModelState.IsValid)
+            {
+
+
+                return RedirectToAction(nameof(Payment), new { traineeid = model.TraineeId, moduleid = model.ModuleId,trainerid = trainer });
+            }
+            return View(model);
+        }
+        public async Task<IActionResult> Payment(string traineeid, int moduleid,string trainerid)
+        {
+            var module = await _trainer.GetModulebyId(moduleid);
+            var trainee = Traineedbcontext.Users.FirstOrDefault(t => t.Id == traineeid);
+            var TraineeViewModel = new Models.AccountViewModels.TraineeViewModel
+            {
+                FirstName = trainee.FirstName,
+                LastName = trainee.LastName,
+                Email = trainee.Email,
+                PhoneNumber = trainee.PhoneNumber,
+                CompanyName = trainee.CompanyName,
+                CompanyAddress = trainee.CompanyAddress,
+                UserAddress = trainee.UserAddress,
+                TraineeId = trainee.Id
+            };
+            ViewBag.Module = module;
+            ViewBag.Trainer = trainerid;
+           // return RedirectToAction(nameof(ViewCertificate), new { traineeid = trainee.UID, moduleid = model.ModuleId, trainerid = trainer });
+            return View(TraineeViewModel);
+        }
+
+        public IActionResult PaymentCompleted(/*RemitaResponse data,*/ string traineeid, string moduleid)
+        {
+            //var training = new Training
+            //{
+            //    PaymentRefId = data.PaymentReference,
+
+
+            //};
+
+            //  var trainee = _traineeuserManager.FindByIdAsync(traineeid);
+            var url = Url.Action(nameof(ViewCertificate), new { traineeid, moduleid });
+            return Json(new { isSuccess = true, redirectUrl = url });
+            //  return RedirectToAction(nameof(ViewCertificate), new {  traineeid, moduleid });
+        }
+
+        public async Task<IActionResult> GenerateCertificate(string traineeid, string moduleid,string trainer)
+        {
+            var user = await _usermanager.FindByIdAsync(trainer);
+
+            var trainee = await _traineeuserManager.FindByIdAsync(traineeid);
+            var train = dbcontext.Trainings.ToList();
+            var training = train.FirstOrDefault(t => t.TraineeId == trainee.UID & t.ModuleId == moduleid & t.TrainingCentreId == user.Id);
+
+            var centre = await _usermanager.FindByIdAsync(user.Id);
+
+
+            var module = admindbcontext.Modules.FirstOrDefault(m => m.Id == int.Parse(moduleid));
+
+            if (training.CertificateId != null)
+            {
+                return Content("Cert already Generated");
+            }
+
+            var CertId = "MISTDO/" + module.ShortCode + "/" + DateTime.Now.Year + "/" + Helpers.GetCertId.RandomString(5);
+
+            ViewBag.Trainee = trainee;
+            ViewBag.Centre = centre;
+            ViewBag.Module = module;
+
+
+
+            var updateTraining = new Training
+            {
+                CertGenDate = DateTime.Now,
+                CertificateId = CertId,
+                DateCreated = training.DateCreated,
+                Id = training.Id,
+                ModuleId = training.ModuleId,
+                PaymentRefId = training.PaymentRefId,
+                TraineeId = training.TraineeId,
+                TrainingCentreId = training.TrainingCentreId,
+                TrainingEndDate = training.TrainingEndDate,
+                TrainingName = training.TrainingName,
+                TrainingStartDate = training.TrainingStartDate,
+
+            };
+
+            var local = dbcontext.Set<Training>()
+    .Local
+    .FirstOrDefault(entry => entry.Id == updateTraining.Id);
+
+            // check if local is not null 
+            if (local != null) // I'm using a extension method
+            {
+                // detach
+                dbcontext.Entry(local).State = EntityState.Detached;
+            }
+            // set Modified flag in your entry
+            dbcontext.Entry(updateTraining).State = EntityState.Modified;
+
+            dbcontext.SaveChanges();
+            // save 
+            Certificate certificate = new Certificate
+            {
+                CertNumber = updateTraining.CertificateId,
+                CertStatus = "Valid",
+                DateGenerated = updateTraining.CertGenDate,
+                Owner = traineeid,
+                Trainer = centre,
+                TrainerOrg = centre.CentreName,
+                TrainerOrgId = user.Id,
+                ModuleId = int.Parse(moduleid),
+                TrainingId = updateTraining.Id
+                //  Course = module,
+                //  Training = training
+
+            };
+
+            dbcontext.Add(certificate);
+
+            await dbcontext.SaveChangesAsync();
+            // Then Send Mail
+            SmtpClient client = new SmtpClient("smtp.office365.com"); //set client 
+            client.Port = 587;
+            client.EnableSsl = true;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential("Wragbydev@wragbysolutions.com", "@Devops19"); //Mailing credential
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress("Wragbydev@wragbysolutions.com");
+            mailMessage.To.Add("Femi4god2010@gmail.com"); //swap with trainee.Email on go live
+            mailMessage.Body = "Dear " + trainee.FirstName + ", Congratulations!! You have been Awarded a Certifcate on " + module.Name + " by " + user.CompanyName + " on " + DateTime.Now + " Go to your Training Provider to Collect your Certificate.";
+            mailMessage.Subject = "MISTDO Certificate ";
+            client.Send(mailMessage);
+
+
+            ViewBag.Training = updateTraining;
+
+
+
+            return View();
+        }
+
         public async Task<IActionResult> AllRegisteredTrainees()
         {
 
