8c200ce2392929e5326767548edf7a80f73d5cb3
Ibrahim Oluwadamilare <ibrahimoluwadamilare01@outlook.com>
2019-05-21 10:01:30 +0100

    updated

--- MISTDO.Web/Services/Ogisp.cs
+++ MISTDO.Web/Services/Ogisp.cs
@@ -37,8 +37,6 @@ namespace MISTDO.Web.Services
                     var strResult = await response.Content.ReadAsStringAsync();
                     if (response.IsSuccessStatusCode)
                     {
-
-
                         return JsonConvert.DeserializeObject<OgispResponse>(strResult);
 
                     }
